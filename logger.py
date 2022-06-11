fbe8a630e097bd6d490470156265c417229725a5
KirillMerz <kiri11.mer31yak0v@gmail.com>
2022-06-11 20:38:17 +0700

    Fix daily log file change

--- logger.py
+++ logger.py
@@ -1,18 +1,25 @@
 import logging
 import os
 from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
 
 LOGS_DIR_NAME = 'logs'
-LOG_FILE_NAME = f'{datetime.now().strftime("%m.%d.%Y")}.log'
 
 if not os.path.exists(LOGS_DIR_NAME):
     os.mkdir(LOGS_DIR_NAME)
 
-logging.basicConfig(
-    filename=f'{LOGS_DIR_NAME}/{LOG_FILE_NAME}',
-    format='[%(asctime)s] %(message)s',
-    datefmt='%H:%M:%S',
+handler = TimedRotatingFileHandler(
+    f'{LOGS_DIR_NAME}/{datetime.now().strftime("%d.%m.%Y")}.log',
+    when='midnight'
 )
 
+formatter = logging.Formatter(
+    '[%(asctime)s] %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+handler.setFormatter(formatter)
+
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
